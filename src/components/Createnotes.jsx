8ffbe7cc219354490a2e81710d6acfe0add4fc12
keerthanarajendran123 <keerthurb9@gmail.com>
2023-12-19 15:49:36 +0530

    added notes button

--- src/components/Createnotes.jsx
+++ src/components/Createnotes.jsx
@@ -1,5 +1,31 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addNote } from '../redux/notesSlice';
+
 function Createnotes() {
+  const dispatch = useDispatch();
+  const [noteTitle, setNoteTitle] = useState('');
+  const [noteContent, setNoteContent] = useState('');
+
+  const handleAddNote = (e) => {
+    e.preventDefault();
+    if (noteTitle.trim() !== '' && noteContent.trim() !== '') {
+      dispatch(
+        addNote({
+          id: Date.now(), 
+          title: noteTitle,
+          content: noteContent,
+          lastUpdate:"Just now", 
+        })
+      );
+      setNoteTitle('');
+      setNoteContent('');
+    }
+    else {
+      alert("Please enter both title and content for the note.");
+    }
+  };
+
   return (
     <div className="container my-4">
       <div className="row justify-content-center">
@@ -7,23 +33,32 @@ function Createnotes() {
           <div className="card">
             <div className="card-body">
               <h2 className="card-title">Add a note</h2>
-              <form>
+              <form onSubmit={handleAddNote}>
                 <div className="form-group">
-                  <label For="noteTitle"></label>
+                  <label htmlFor="noteTitle"></label>
                   <input
                     type="text"
                     className="form-control"
                     id="noteTitle"
-                    placeholder="Title"/>
+                    placeholder="Title"
+                    value={noteTitle}
+                    onChange={(e) => setNoteTitle(e.target.value)}
+                  />
                 </div>
                 <div className="form-group">
-                  <label For="noteContent"></label>
+                  <label htmlFor="noteContent"></label>
                   <textarea
                     className="form-control"
                     id="noteContent"
                     rows="4"
-                    placeholder="Take a note..."></textarea>
+                    placeholder="Take a note..."
+                    value={noteContent}
+                    onChange={(e) => setNoteContent(e.target.value)}
+                  ></textarea>
                 </div>
+                <button type="submit" className="btn btn-primary">
+                  Add notes
+                </button>
               </form>
             </div>
           </div>
@@ -34,4 +69,3 @@ function Createnotes() {
 }
 
 export default Createnotes;
-
